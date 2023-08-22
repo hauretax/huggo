e342ecf3230f77819cb3d889cc40f8820bf0594f
Huggo <31890474+hauretax@users.noreply.github.com>
2023-08-22 19:56:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+Je suis vivement intéressé par la perspective de participer à de nouveaux projets au sein d'AXOPEN. Fort d'une année d'expérience en tant que conseiller développeur web, où j'ai travaillé avec PHP et JavaScript, j'ai pu acquérir une solide compréhension des technologies clés.
+Mon enthousiasme découle de ma passion pour l'exploration et la concrétisation de nouvelles initiatives.. J'aspire à apporter une contribution positive au monde à travers mon travail.<br/><br/>
+
+Mon expérience antérieure m'a permis de développer des compétences spécifiques, notamment la capacité à résoudre des situations complexes et difficiles. Mon aptitude à l'écoute s'est avérée essentielle pour identifier et répondre aux besoins uniques de chaque projet. De plus, ma formation à l'école 42 a renforcé ma capacité à argumenter de manière convaincante pour défendre mes idées, tout en me permettant de reconnaître avec humilité lorsque je prends une mauvaise direction.<br/><br/>
+
+Intégrer l'équipe AXOPEN serait pour moi une opportunité passionnante de continuer à développer mon expertise tout en contribuant activement à des projets innovants. Je suis impatient de discuter davantage de la façon dont je pourrais apporter une perspective unique à vos équipes et participer activement à la réalisation de vos objectifs.<br/><br/>
+
+Cordialement,<br/>[Votre nom]
+
+
+
     </div>
   );
 }
